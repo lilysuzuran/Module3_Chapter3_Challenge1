fea475bdf26e31eef82480e9baf66fea69bdac06
LKimABK <109537516+lilysuzuran@users.noreply.github.com>
2022-08-03 13:59:00 -0700

    Recreated the hello world example code

---
+++ Main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+ bool ShouldEndThread = false;
+
+ int ThreadVar = 0;
+
+ bool DidQuit = false;
+
+void HelloWorldThread()
+{
+    while (!DidQuit)
+    {
+        ThreadVar++;
+        if (ThreadVar > 1000)
+        {
+            ThreadVar = 0;
+        }
+    }
+}
+
+int main()
+{
+    cout << "hello world" << endl;
+    char UserInput;
+    std::thread Hello(HelloWorldThread);
+
+
+    while (!DidQuit)
+    {
+        cout << "enter any key to display counter" << endl;
+        cout << "q to quit" << endl;
+        cin >> UserInput;
+        DidQuit = (UserInput == 'q');
+        cout << "ThreadVar: " << ThreadVar << endl;
+    }
+
+
+    Hello.join();
+
+    return 0;
+}
